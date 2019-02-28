efb4345ee394064a01571d2ed123f73b7962d578
xinkai.wu@tescomm.com.cn <xinkai.wu@tescomm.com.cn>
2019-02-28 17:45:31 +0800

    -吗m1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sass 学习
+## 变量
+```
+$color: red;
+$fontSize: 50;
+$text: ".b";
+
+.a {
+  color: red;
+
+  #$text} {
+    font-size: #{$fontSize}px;
+  }
+}
+```
+# 
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+   <div class="a">
+      {{msg}}
+    <div class="b">
+      这是一个很不错的选择
+    </div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'This is a good choice.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
+<style lang="scss" scoped >
+//------------sass语言--------------------
+
+$color: red;
+$fontSize: 50;
+$text: ".b";
+.a {
+  color: red;
+  #{$text} {
+    font-size: #{$fontSize}px;
+  }
+}
+
+//------------------ 原始写法 ----------------------------
+// .a {
+//   color: red;
+// }
+// .a .b {
+//   font-size: 50px;
+//   background: pink;
+// }
+</style>
